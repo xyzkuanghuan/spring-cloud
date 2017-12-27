e9bda610f040bd9a3df9bb777ffea6f88dfa311d
kuanghuan <huankuang@alog.com>
2017-12-27 16:06:04 +0800

    服务注册与发现学习
    Signed-off-by: kuanghuan <huankuang@alog.com>

---
+++ eureka-client/src/main/java/com/xyz/spring/boot/cloud/DcController.java
@@ -0,0 +1,24 @@
+package com.xyz.spring.boot.cloud;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by 10058393 on 2017/12/27.
+ */
+@RestController
+public class DcController {
+
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @GetMapping("/dc")
+    public String dc() {
+        String services = "Services: " + discoveryClient.getServices();
+        System.out.println(services);
+        return services;
+    }
+
+}
---
+++ eureka-server/src/main/java/com/xyz/spring/boot/cloud/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.xyz.spring.boot.cloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(EurekaServerApplication.class).web(true).run(args);
+        // SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
