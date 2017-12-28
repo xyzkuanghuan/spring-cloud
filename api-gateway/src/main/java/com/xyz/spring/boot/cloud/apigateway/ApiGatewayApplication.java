fd7f6b8f5aaef48be6359496227123eddbc99155
kuanghuan <huankuang@alog.com>
2017-12-28 16:53:04 +0800

    api-gate,config-client,config-server,config-repo
    Signed-off-by: kuanghuan <huankuang@alog.com>

---
+++ api-gateway/src/main/java/com/xyz/spring/boot/cloud/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.xyz.spring.boot.cloud.apigateway;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ApiGatewayApplication.class).web(true).run(args);
+	}
+}
